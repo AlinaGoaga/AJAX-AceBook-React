fe8ceb4dd8b3b9f628b1d44b57f5efb2fb8b58c1
Alina Goaga <goaga.alina@gmail.com>
2019-02-10 15:07:53 +0000

    Made user logout work

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,8 @@ class ApplicationController < ActionController::Base
   private
 
   # Overwriting the sign_out redirect path method
-  def after_sign_out_path_for(resource_or_scope)
-    home_path
+  def after_sign_out_path_for(_resource_or_scope)
+    api_v1_users_check_for_user
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users do
-    get '/users/sign_out' => 'devise/sessions#destroy'
-  end
+  devise_for :users, controllers: { sessions: 'sessions' }
 
   root to: 'home#index'
 
